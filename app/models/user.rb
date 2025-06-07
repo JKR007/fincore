162378877f9b739742b60ec284c570a9ef9d7ce5
AbduQahhor <abduqahhor.muhamadjonov@gmail.com>
2025-06-08 02:45:43 +0500

    add transactions model, add specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  has_many :transactions, dependent: destroy
+
   validates :email, presence: true,
                     uniqueness: { case_sensitive: false },
                     format: { with: URI::MailTo::EMAIL_REGEXP }
